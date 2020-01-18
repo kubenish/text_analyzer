6c12f0a4cd85881afb00730174895eb2513356bc
Francja <adambielewicz92@gmail.com>
2020-01-18 13:00:12 +0100

    minor esthetic changes to code

--- text_analyzer.py
+++ text_analyzer.py
@@ -16,8 +16,7 @@ def menu():
           "6. Generate report",
           "7. Save statistics to file",
           "8. Close the application",
-          sep="\n"
-          )
+          sep="\n")
     return input("Select option: ")
 
 
@@ -41,24 +40,24 @@ def count_words(text):
 
 
 def count_punctuation(text):
-    num_punct = 0
-    for x in text:
-        if x in PUNCTUATION:
-            num_punct += 1
-    return num_punct
+    num_punctuation = 0
+    for char in text:
+        if char in PUNCTUATION:
+            num_punctuation += 1
+    return num_punctuation
 
 
 def get_report(text):
-    for c in string.ascii_uppercase:
+    for char in string.ascii_uppercase:
         counter = Counter(text.upper())
-        print("%s: %i" % (c, counter[c]))
+        print("%s: %i" % (char, counter[char]))
 
 
 if __name__ == "__main__":
     while True:
         try:
             choice = int(menu())
-            if choice<1 or choice>8:
+            if choice < 1 or choice > 8:
                 print("ERROR: You typed incorrect number. Select option by typing 1-8.")
                 continue
         except ValueError:
@@ -70,24 +69,24 @@ if __name__ == "__main__":
         elif 2 <= choice <= 6:
             try:
                 with open(FILENAME, 'r') as file:
-                    text = file.read()
+                    text_from_file = file.read()
             except FileNotFoundError:
                 print("ERROR: File %s not found. Please download the file first." % FILENAME)
                 continue
             if choice == 2:
-                letters = count_letters(text)
+                letters = count_letters(text_from_file)
                 print("Number of letters in the file: %i" % letters)
             elif choice == 3:
-                words = count_words(text)
+                words = count_words(text_from_file)
                 print("Number of words in the file: %i" % words)
             elif choice == 4:
-                punctuations = count_punctuation(text)
+                punctuations = count_punctuation(text_from_file)
                 print("Number of punctuation marks in the file: %i" % punctuations)
             elif choice == 5:
                 print("Selected option %s not yet implemented" % choice)  # TO DO, REPLACE AFTER IMPLEMENTATION
             elif choice == 6:
                 print("Number of occurrences of each letter in the text:")
-                get_report(text)
+                get_report(text_from_file)
         elif choice == 7:
             print("Selected option %s not yet implemented" % choice)  # TO DO, REPLACE AFTER IMPLEMENTATION
         elif choice == 8:
